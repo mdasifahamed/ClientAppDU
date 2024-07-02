dcdd749736fb64f037ec4ba0f0dda04cacd2f59e
mdasifahamed <mdasifahamed89@yahoo.com>
2024-07-02 20:26:57 +0600

    Update
    Now Issue Certificate Send Email to The Student  On Success Issuance

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const crypto = require('crypto')
 const contract = require('./contract')
+const sendEmail = require('./sendEmail.js')
 const app = exrpress()
 const port = 8001
 app.use(bodyParser.json())
@@ -21,21 +22,28 @@ app.post('/issue-certificate', cors(corsOptions),async(req,res)=>{
     let cert_hash  = crypto.createHash('sha256')
     let cert_id = Math.floor(Math.random() * 12562) * Math.floor(Math.random() * 25641);
     let track_id = req.body.tracking_id;
+    let student_name,student_email;
 
     try {
+
         request = await contract.read_request(track_id.toString())
         request = JSON.parse(request)
-        request.Certificate_Id = cert_id
+        student_name = request.Student_Name
+        student_email = request.Student_Email
         let newRequest = JSON.stringify(request)
-        cert_hash = cert_hash.update(newRequest)
+        cert_hash = cert_hash.update(`${newRequest.Student_Name},${newRequest.Student_Id},${newRequest.Student_Email},${newRequest.Degree},${newRequest.Major},${newRequest.Result},${newRequest.Requester_Authority},${cert_id}`);
         cert_hash = cert_hash.digest('hex')
+ 
+       
     } catch(error) {
         if (error){
+            console.log(error)
             return res.status(500).json({data:`No data found for  id ${track_id}`})
         }
     }
 
     try {
+       
         let result = await contract.issue_certificate(track_id.toString(),cert_hash,cert_id.toString())
         if (parseInt(result) === 0 ){
             return res.status(200).json({data: `Certificate Already Created For The Tracking Id ${track_id}`})
@@ -43,9 +51,16 @@ app.post('/issue-certificate', cors(corsOptions),async(req,res)=>{
         if( result === "Something Went Wrong"){
             return res.status(500).json({data:"Failed To Connect The Blokchain Network"})
         }
+        let emailStatus = await sendEmail(student_name,track_id,student_email)
+        if (emailStatus === false) {
+
+            return res.status(500).json({data:`Certificate Created With The Certificate Id ${result}. But Email Was not Sent`})
+            
+        }
         return res.status(201).json({data: `Certificate Created With The Certificate Id ${result}`})
     } catch (error) {
         if (error){
+            console.log(error)
             return res.status(500).json({data:`No data found for  id ${track_id}`})
         }
     }
@@ -162,6 +177,36 @@ app.post('/verify-by-hash', cors(corsOptions), async(req,res)=>{
 
         if (error) {
             return res.status(500).json({data:`Certificate Not Found For The Hash ${certificate_hash}`})
+        }
+    }
+})
+
+
+
+app.get('/history-of-certificate/:tracking_id', cors(corsOptions),async (req,res)=>{
+
+    let tracking_id = req.params.tracking_id
+    if(!tracking_id){
+        return res.status(400).json({data:"Required Fields Tracking_Id  Is  Missing"})
+    }
+
+    try {
+        let request_history =  await contract.history_of_a_request(tracking_id.toString())
+        let requests = JSON.parse(request_history)
+        requests.forEach(request =>{
+            if(request.Is_Reqeust_Completed){
+                request.Requester_Authority = "Dhaka College"
+                request.Issuer_Authority = "Dhaka Univertsity"
+            } else {
+                request.Requester_Authority = "Dhaka College"
+            }
+        })
+        
+        return res.status(200).json({data:requests})
+
+    } catch (error) {
+        if (error) {
+            return res.status(500).json({data:`Certficate History Is Not Found For The Tracking Id : ${tracking_id}`})
         }
     }
 })
